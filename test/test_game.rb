e5b487118ced329a147d4f46ee193e7f0cc9a704
Damien Pollet <damien.pollet@gmail.com>
2014-08-08 18:09:58 +0200

    Include module for concision

--- test/test_game.rb
+++ test/test_game.rb
@@ -2,13 +2,15 @@ require_relative 'helper'
 require 'uno/game'
 require 'uno/play'
 
+include Uno
+
 describe 'a new game of 3 players' do
 
   before do
-    @game = Uno::Game.new
-    @a = Uno::Player.new 'A'
-    @b = Uno::Player.new 'B'
-    @c = Uno::Player.new 'C'
+    @game = Game.new
+    @a = Player.new 'A'
+    @b = Player.new 'B'
+    @c = Player.new 'C'
     @game.add_player @a
     @game.add_player @b
     @game.add_player @c
@@ -17,7 +19,7 @@ describe 'a new game of 3 players' do
   describe 'when the reveal is a number' do
 
     before do
-      @game.reveal Uno::Play.new(5, 'red')
+      @game.reveal Play.new(5, 'red')
     end
 
     it 'should have a correct first player after reveal' do
@@ -25,14 +27,14 @@ describe 'a new game of 3 players' do
     end
 
     it 'should have a correct second player' do
-      @game.play Uno::Play.new(6, 'red', 'A')
+      @game.play Play.new(6, 'red', 'A')
       @game.expected_player.must_be_same_as @b
     end
 
     it 'should come back to first player in 3 plays' do
-      @game.play Uno::Play.new(6, 'red', 'A')
-      @game.play Uno::Play.new(7, 'red', 'B')
-      @game.play Uno::Play.new(8, 'red', 'C')
+      @game.play Play.new(6, 'red', 'A')
+      @game.play Play.new(7, 'red', 'B')
+      @game.play Play.new(8, 'red', 'C')
 
       @game.expected_player.must_be_same_as @a
     end
