868f5b071e57d9de1d0a3f657a435271bd79b79a
Damien Pollet <damien.pollet@gmail.com>
2014-08-08 17:37:26 +0200

    Test simple plays

--- test/test_game.rb
+++ test/test_game.rb
@@ -14,14 +14,28 @@ describe 'a new game of 3 players' do
     @game.add_player @c
   end
 
-  it 'should have a correct first player after reveal' do
-    @game.reveal Uno::Play.new(7, 'red')
-    @game.expected_player.must_be_same_as @a
-  end
+  describe 'when the reveal is a number' do
 
-  it 'should have a correct second player' do
-    @game.reveal Uno::Play.new(7, 'red')
-    @game.play Uno::Play.new(8, 'red', 'A')
-    @game.expected_player.must_be_same_as @b
+    before do
+      @game.reveal Uno::Play.new(5, 'red')
+    end
+
+    it 'should have a correct first player after reveal' do
+      @game.expected_player.must_be_same_as @a
+    end
+
+    it 'should have a correct second player' do
+      @game.play Uno::Play.new(6, 'red', 'A')
+      @game.expected_player.must_be_same_as @b
+    end
+
+    it 'should come back to first player in 3 plays' do
+      @game.play Uno::Play.new(6, 'red', 'A')
+      @game.play Uno::Play.new(7, 'red', 'B')
+      @game.play Uno::Play.new(8, 'red', 'C')
+
+      @game.expected_player.must_be_same_as @a
+    end
+
   end
 end
